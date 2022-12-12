abb23e26ad56cb5bd7230bf51bcea02c811f26e9
sjwys <sjwyss@outlook.com>
2022-12-12 15:41:15 -0500

    Crude first version

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
 import fs from "fs";
 
+//TODO: Clean up and optimize code
+//TODO: Look into target directory and look for last file name so the rename to move command also names it appropriately, does it need randomized at all if this is done?
+
 function randomString() {
     return new Promise((resolve, reject) => {
         resolve(Math.random().toString(36).slice(2));
     });
 }
 
-function randomizeFileName(path, randName) {
+function getNewFileAttributes(path, randName) {
     return new Promise((resolve, reject) => {
         let pathArray = path.split("/");
         let parentDirectory = pathArray.slice(0, -1).join("/");
         let fileName = pathArray.pop(1);
         let fileExtension = fileName.split(".").pop(1);
-        let newPath = parentDirectory + "/" + randName + "." + fileExtension;
-        resolve(newPath);
+        let combinedFileName = randName + "." + fileExtension;
+        let newPath = parentDirectory + "/" + combinedFileName;
+        const fileAttributes = {
+            path: newPath,
+            name: combinedFileName,
+        };
+        resolve(fileAttributes);
     });
 }
 
@@ -34,30 +42,39 @@ async function organizeFile(filePath, targetDir) {
     try {
         const file = await fs.promises.stat(filePath);
         const randName = await randomString();
-        const newFilePath = await randomizeFileName(filePath, randName);
-        await fs.promises.rename(filePath, newFilePath);
+        const newFileAttributes = await getNewFileAttributes(
+            filePath,
+            randName
+        );
+        await fs.promises.rename(filePath, newFileAttributes.path);
         const fileDateDict = await getFileDateDict(file);
         const targetYearPath = targetDir + "/" + fileDateDict.modYear;
-        const targetMonthPath = targetYearPath + "/" + fileDateDict.modMon;
-        if (fs.existsSync(targetYearPath)) {
-            if (fs.existsSync(targetMonthPath)) {
+        const targetMonthPath =
+            targetYearPath +
+            "/" +
+            fileDateDict.modYear +
+            "-" +
+            fileDateDict.modMon;
+
+        try {
+            if (fs.existsSync(targetYearPath)) {
+                if (fs.existsSync(targetMonthPath)) {
+                } else {
+                    await fs.promises.mkdir(targetMonthPath);
+                }
             } else {
-                //make month directory
-                // move file
+                await fs.promises.mkdir(targetYearPath);
+                await fs.promises.mkdir(targetMonthPath);
             }
-        } else {
-            //make year directory
-            //make month directory
-            // move file
+        } catch (err) {
+            console.log(`${targetMonthPath} already exists, moving file`);
+        } finally {
+            await fs.promises.rename(
+                newFileAttributes.path,
+                targetMonthPath + "/" + newFileAttributes.name
+            );
         }
-        //rename file
     } catch (err) {
         console.log(err);
     }
-    console.log("done");
 }
-
-organizeFile(
-    "C:/Users/sjwys/Documents/Projects/FileMan/test.txt",
-    "C:/Users/sjwys/Documents/Projects/FileMan/target"
-);
