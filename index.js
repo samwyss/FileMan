de0796435248c6dd6d6211fa19997d11ab2f1164
Sam Wyss <sjwyss@outlook.com>
2022-12-19 15:53:33 -0500

    first working version, need to reimplement async

--- index.js
+++ index.js
@@ -1,28 +1,26 @@
-import fs, { rename } from "fs";
+import fs from "fs";
 
 function getFileAttributes(path) {
-    return new Promise((resolve, reject) => {
-        try {
-            const file = fs.statSync(path); //returns information about the file located at "path"
-            const modDate = file.mtime.toISOString().split("-"); // returns an array with both the year and month of last modification
-            const pathArray = path.split("/"); // array whose elements consist of subdirectories leading to the path of the file
-            const fullFileNameArr = pathArray.pop(1).split("."); // an array that contains the file name and extension in the form of ["name" "extension"]
-            const name = fullFileNameArr[0]; // the name of the file
-            const extension = "." + fullFileNameArr[1]; // the file extension including the "." character
-            const parentDir = pathArray.join("/"); // the parent directory to the file
-            const fileAttributes = {
-                path: path,
-                parentDir: parentDir,
-                name: name,
-                extension: extension,
-                modYear: modDate[0],
-                modMon: modDate[1],
-            }; // object containing all relevant file information
-            resolve(fileAttributes);
-        } catch (err) {
-            reject(console.error(`Unable to obtain attributes of ${path}`)); // returns an error if the attributes of "path" cannot be obtained
-        }
-    });
+    try {
+        const file = fs.statSync(path); //returns information about the file located at "path"
+        const modDate = file.mtime.toISOString().split("-"); // returns an array with both the year and month of last modification
+        const pathArray = path.split("/"); // array whose elements consist of subdirectories leading to the path of the file
+        const fullFileNameArr = pathArray.pop(1).split("."); // an array that contains the file name and extension in the form of ["name" "extension"]
+        const name = fullFileNameArr[0]; // the name of the file
+        const extension = "." + fullFileNameArr[1]; // the file extension including the "." character
+        const parentDir = pathArray.join("/"); // the parent directory to the file
+        const fileAttributes = {
+            path: path,
+            parentDir: parentDir,
+            name: name,
+            extension: extension,
+            modYear: modDate[0],
+            modMon: modDate[1],
+        }; // object containing all relevant file information
+        return fileAttributes;
+    } catch (err) {
+        reject(console.error(`Unable to obtain attributes of ${path}`)); // returns an error if the attributes of "path" cannot be obtained
+    }
 }
 
 function isDir(path) {
@@ -42,37 +40,33 @@ function isFile(path) {
 }
 
 function setRandomFileName(fileAttributes) {
-    return new Promise((resolve, reject) => {
-        try {
-            const randomName = Math.random().toString(36).slice(2); // a random file name to be used while moving files to avoid path conflicts
-            fs.renameSync(
-                fileAttributes.path,
-                fileAttributes.parentDir +
-                    "/" +
-                    randomName +
-                    fileAttributes.extension
-            ); // rename the file using the randomly generated name
-            fileAttributes.name = randomName; // update fileAttributes object to contain updated name
-            fileAttributes.path =
-                fileAttributes.parentDir +
+    try {
+        const randomName = Math.random().toString(36).slice(2); // a random file name to be used while moving files to avoid path conflicts
+        fs.renameSync(
+            fileAttributes.path,
+            fileAttributes.parentDir +
                 "/" +
                 randomName +
-                fileAttributes.extension; // update fileAttributes object to contain updated path
-            resolve(fileAttributes);
-        } catch (err) {
-            reject(
-                console.error(
-                    `Unable to randomize name of file ${fileAttributes.path}`
-                ) // returns if the file represented by fileAttributes cannot be renamed
-            );
-        }
-    });
+                fileAttributes.extension
+        ); // rename the file using the randomly generated name
+        fileAttributes.name = randomName; // update fileAttributes object to contain updated name
+        fileAttributes.path =
+            fileAttributes.parentDir +
+            "/" +
+            randomName +
+            fileAttributes.extension; // update fileAttributes object to contain updated path
+        return fileAttributes;
+    } catch (err) {
+        console.error(
+            `Unable to randomize name of file ${fileAttributes.path}`
+        ); // returns if the file represented by fileAttributes cannot be renamed
+    }
 }
 
-async function organizeFile(filePath, targetDir) {
+function organizeFile(filePath, targetDir) {
     try {
-        let fileAttributes = await getFileAttributes(filePath); // returns an object containing relevant file information
-        fileAttributes = await setRandomFileName(fileAttributes); // randomizes the file name to avoid path conflicts and updates fileAttributes object
+        let fileAttributes = getFileAttributes(filePath); // returns an object containing relevant file information
+        fileAttributes = setRandomFileName(fileAttributes); // randomizes the file name to avoid path conflicts and updates fileAttributes object
         const targetYearPath = targetDir + "/" + fileAttributes.modYear; // the path corresponding to the directory in "targetDir" corresponding to the year the file was last modified
         const targetMonthPath =
             targetYearPath +
@@ -90,7 +84,7 @@ async function organizeFile(filePath, targetDir) {
             fs.mkdirSync(targetYearPath); // creates the year directory if neither "targetYearPath" or "targetMonthPath" exists in "targetDir"
             fs.mkdirSync(targetMonthPath); // creates the month directory if neither "targetYearPath" or "targetMonthPath" exists in "targetDir"
         }
-        await fs.promises.rename(
+        fs.renameSync(
             fileAttributes.path,
             targetMonthPath +
                 "/" +
@@ -151,66 +145,68 @@ function getLargestOrganizedFileNumber(fileAttributes, largestFileNumber) {
 }
 
 function renameFileQueue(queue, num) {
-    return new Promise((resolve, reject) => {
-        try {
-            num = Number(num);
-            while (queue.length) {
-                num += 1;
-                let file = queue.shift();
-                let newName =
-                    file.parentDir +
-                    "/" +
-                    file.modYear +
-                    "-" +
-                    file.modMon +
-                    "_" +
-                    num +
-                    file.extension;
-                fs.renameSync(file.path, newName);
-            }
-        } catch (err) {
-            console.err(err);
+    try {
+        num = Number(num);
+        while (queue.length) {
+            num += 1;
+            let file = queue.shift();
+            let newName =
+                file.parentDir +
+                "/" +
+                file.modYear +
+                "-" +
+                file.modMon +
+                "_" +
+                num +
+                file.extension;
+            fs.renameSync(file.path, newName);
         }
-    });
+    } catch (err) {
+        console.err(err);
+    }
 }
 
-async function verifyOrganizedFileNames(targetDir) {
+function verifyOrganizedDirectoryNames(files, month) {
+    var renameQueue = [];
+    var largestOrganizedFileNumber = -1;
+    for (let k in files) {
+        const file = `${month}/${files[k]}`; // absolute path to a file in "targetDir/year/month"
+        let fileAttributes = getFileAttributes(file);
+        if (isValidName(fileAttributes)) {
+            largestOrganizedFileNumber = getLargestOrganizedFileNumber(
+                fileAttributes,
+                largestOrganizedFileNumber
+            );
+        } else {
+            renameQueue.push(fileAttributes);
+        }
+    }
+    renameFileQueue(renameQueue, largestOrganizedFileNumber);
+}
+
+function organizedDirectoryNavigator(targetDir) {
     try {
         const years = fs.readdirSync(targetDir); // returns an array containing all year directories in "targetDir"
+        console.log(years);
         for (let i in years) {
             const year = `${targetDir}/${years[i]}`; // absolute path to a year directory in "targetDir"
             const months = fs.readdirSync(year);
             for (let j in months) {
                 const month = `${year}/${months[j]}`; // absolute path to a month directory in "targetDir/year"
                 const files = fs.readdirSync(month);
-                var renameQueue = [];
-                var largestOrganizedFileNumber = -1;
-                for (let k in files) {
-                    const file = `${month}/${files[k]}`; // absolute path to a file in "targetDir/year/month"
-                    let fileAttributes = await getFileAttributes(file); //TODO: make a synchronous version of this function? Look into screenshot, something may be able to be put into a function such that a return is waited for
-                    if (isValidName(fileAttributes)) {
-                        largestOrganizedFileNumber =
-                            getLargestOrganizedFileNumber(
-                                fileAttributes,
-                                largestOrganizedFileNumber
-                            );
-                    } else {
-                        renameQueue.push(fileAttributes);
-                    }
-                    //TODO: between this and above todo needs to be in an await function
-                }
-                await renameFileQueue(renameQueue, largestOrganizedFileNumber);
+                verifyOrganizedDirectoryNames(files, month);
             }
         }
     } catch (err) {
-        console.error(error);
+        console.error(err);
     }
 }
 
 function driver() {
-    unorganizedFileFinder(,);
-
-    verifyOrganizedFileNames();
+    let sPath = ;
+    let tPath = ;
+    unorganizedFileFinder(sPath, tPath);
+    organizedDirectoryNavigator(tPath);
 }
 
-driver(); // something in verifyOrganizedFileNames is returning before
+driver();
